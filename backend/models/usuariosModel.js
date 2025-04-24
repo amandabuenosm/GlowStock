b3041583e69e558115cbcad7cb81fe3c257e72b8
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-04-24 16:55:59 -0300

    Create usuariosModel.js

---
+++ backend/models/usuariosModel.js
@@ -0,0 +1,21 @@
+const database = require('../config/db');
+
+const crudusuarios = {
+    listarusuario: (callback) => {
+        database.query('SELECT * FROM usuarios', callback);
+    },
+    buscapeloid: (id, callback) => {
+        database.query('SELECT * FROM usuarios WHERE id = ?', [id], callback);
+    },
+    criarusuario: (novousuario, callback) => {
+        database.query('INSERT INTO usuarios SET ?', novousuario, callback);
+    },
+    editarusuario: (id, usuarioEdit, callback) => {
+        database.query('UPDATE usuarios SET ? WHERE id = ?', [usuarioEdit, id], callback);
+    },
+    deletarusuario: (id, callback) => {
+        database.query('DELETE FROM usuarios WHERE id = ?', [id], callback);
+    },
+}
+
+module.exports = crudusuarios;
