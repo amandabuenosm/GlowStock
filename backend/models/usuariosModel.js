67d3dfd017ed1078da0c50143ea441d7c426d663
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-04-25 17:39:18 -0300

    Update usuariosModel.js

--- backend/models/usuariosModel.js
+++ backend/models/usuariosModel.js
@@ -4,6 +4,9 @@ const crudusuarios = {
     listarusuario: (callback) => {
         database.query('SELECT * FROM usuarios', callback);
     },
+    buscalogin: (login, senha, callback) => {
+        database.query('SELECT * FROM usuarios WHERE login = ? AND senha = ?', [login, senha], callback)
+    },
     buscapeloid: (id, callback) => {
         database.query('SELECT * FROM usuarios WHERE id = ?', [id], callback);
     },
