6e2e13ebb8b7fffef9a54059f32e927af9a284d4
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-04-29 20:35:16 -0300

    Update MenuPage.js

--- frontend/src/pages/MenuPage.js
+++ frontend/src/pages/MenuPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../style/MenuPage.css';
 
 const MenuPage = () => {
     
@@ -14,24 +16,26 @@ const MenuPage = () => {
             </article>
             
             <article className="sessoes">
-                <div className="sessao-usuarios">
+                <Link to="/usuarios" className="sessao-usuarios">
                     <h3>Usuários</h3>
                     <h5>Faça a manutenção dos usuários de seu sistema de controle de estoque por aqui!</h5>
-                </div>
-                <div className="sessao-produtos">
+                </Link>
+
+                <Link to="/produtos" className="sessao-produtos">
                     <h3>Produtos</h3>
                     <h5>Faça a manutenção dos produtos e o controle de saídas e entradas dos itens do seu estoque!</h5>
-                </div>
-                <div className="sessao-movimentacao">
+                </Link>
+
+                <Link to="/movimentacoes" className="sessao-movimentacao">
                     <h3>Movimentações</h3>
                     <h5>Faça a consulta de movimentações de estoque de determinada referência nessa tela de consulta rápida!</h5>
-                </div>
-                <div className="sessao-relatorios">
-                    <h5>Relatórios</h5>
-                </div>
+                </Link>
+
+                <Link to="/relatorios" className="sessao-relatorios">
+                    <h3>Relatórios</h3>
+                </Link>
             </article>
         </section>
-
     </div>
     );
 }
