3105d14d09d753845c870be3d5412ed00f366bf4
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-05-05 01:45:07 -0300

    Update movimentController.js

--- backend/controllers/movimentController.js
+++ backend/controllers/movimentController.js
@@ -6,8 +6,30 @@ const controllerMov = {
         funcModelMov.movporproduto(idProd, (err, results) => {
             if (err) {
                 return res.status(500).json({ erro: "Erro na busca de movimentações." });
-            } res.status(200).json(results);
-        })
+            }
+            res.status(200).json(results);
+        });
+    },
+
+    armazenamentomovs: (req, res) => {
+        const movimentacao = {
+            produto_id: req.body.produto_id,
+            tipo_movimentacao: req.body.tipo_movimentacao,
+            quantidade: req.body.quantidade,
+            usuario_id: req.body.usuario_id 
+        };
+
+        if (!movimentacao.usuario_id) {
+            return res.status(400).json({ erro: "Usuário não informado!" });
+        }
+
+        funcModelMov.registrarmov(movimentacao, (err, result) => {
+            if (err) {
+                console.error("Erro ao registrar movimentação:", err);
+                return res.status(500).json({ erro: "Erro no registro de movimentações." });
+            }
+            res.status(201).json({ mensagem: "Movimentação registrada com sucesso." });
+        });
     }
 };
 
