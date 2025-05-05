f54593def582bdfff90e23406cfc4e5357e08158
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-05-05 01:47:06 -0300

    Update movimentRoutes.js

--- backend/routes/movimentRoutes.js
+++ backend/routes/movimentRoutes.js
@@ -3,7 +3,6 @@ const rota = express.Router();
 const funcController = require('../controllers/movimentController');
 
 rota.get("/:idProd", funcController.movporproduto);
+rota.post("/", funcController.c);
 
 module.exports = rota;
-
-/** adicionar documentação Swagger */
