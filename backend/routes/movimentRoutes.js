089222ae9a879ada19738d1f245cdc9d9aaf7fd1
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-05-09 02:04:29 -0300

    Update movimentRoutes.js

--- backend/routes/movimentRoutes.js
+++ backend/routes/movimentRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const rota = express.Router();
 const funcController = require('../controllers/movimentController');
 
+rota.get("/", funcController.listarmovimentacoes);
 rota.get("/:idProd", funcController.movporproduto);
 rota.post("/", funcController.armazenamentomovs);
 
