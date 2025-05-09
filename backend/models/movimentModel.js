7b272cfdab94a756ac30dc81cf36f9fc4181114e
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-05-09 02:03:59 -0300

    Update movimentModel.js

--- backend/models/movimentModel.js
+++ backend/models/movimentModel.js
@@ -1,21 +1,54 @@
 const database = require('../config/db');
 
 const consultamov = {
-    // inserir movimentações de estoque no banco de dados
+    
     registrarmov: (movimentacao, callback) => {
         const valores = [
-            movimentacao.produto_id, 
+            movimentacao.produto_id,
             movimentacao.tipo_movimentacao,
             movimentacao.quantidade,
             movimentacao.usuario_id,
         ];
 
-        database.query('INSERT INTO movimentacoes (produto_id, tipo_movimentacao, quantidade, data_hora, usuario_id) VALUES (?, ?, ?, NOW(), ?)', valores, callback);
+        database.query(
+            'INSERT INTO movimentacoes (produto_id, tipo_movimentacao, quantidade, data_hora, usuario_id) VALUES (?, ?, ?, NOW(), ?)',
+            valores,
+            callback
+        );
     },
 
-    // buscar movimentações de apenas um produto
     movporproduto: (idProduto, callback) => {
-        database.query('SELECT mov.tipo_movimentacao, mov.quantidade, mov.data_hora, users.nomecomp FROM movimentacoes mov JOIN usuarios users ON mov.usuario_id = users.id WHERE mov.produto_id = ? ORDER BY mov.data_hora DESC', idProduto, callback);
+        database.query(
+            `SELECT 
+                prod.nome AS produtos,
+                mov.tipo_movimentacao,
+                mov.quantidade,
+                mov.data_hora,
+                users.nomecomp AS usuarios
+            FROM movimentacoes mov
+            JOIN usuarios users ON mov.usuario_id = users.id
+            JOIN produtos prod ON mov.produto_id = prod.id
+            WHERE mov.produto_id = ?
+            ORDER BY mov.data_hora DESC`,
+            [idProduto], callback
+        );
+    },
+
+    listarmovimentacoes: (callback) => {
+        database.query(
+            `SELECT 
+                mov.id, 
+                prod.nome AS produtos, 
+                mov.tipo_movimentacao, 
+                mov.quantidade, 
+                mov.data_hora, 
+                users.nomecomp AS usuarios
+            FROM movimentacoes mov
+            JOIN produtos prod ON mov.produto_id = prod.id
+            JOIN usuarios users ON mov.usuario_id = users.id
+            ORDER BY mov.data_hora DESC`,
+            callback
+        );
     },
 };
 
